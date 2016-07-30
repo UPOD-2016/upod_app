3a665b0b692e753fd75d1c237a20dbd10740090a
rmoro <moro1422@mylaurier.ca>
2016-07-29 20:53:18 -0400

    cleaned and commented

--- app/assets/javascripts/resize-header.js
+++ app/assets/javascripts/resize-header.js
@@ -1,39 +1,33 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-$(function() {
-  var arrowWidth = 30;
+// Resize the Select boxes in search forms to match the size of the highlighted
+// (selected) text.
+// Source was originally taken from
+// https://stackoverflow.com/questions/28308103/adjust-width-of-select-element-according-to-selected-options-width
+// It was then modified to work on page load and with turbolinks. Redundant fn calls were taken out.
+// RM
 
+$(function() {
   $.fn.resizeselect = function(settings) {
     return this.each(function() {
-
       $(this).change(function() {
         var $this = $(this);
+        var selected_text = $this.find("option:selected").text();
+        var $temp_element = $("<span>").html(selected_text);
 
-        // create test element
-        var text = $this.find("option:selected").text();
-        var $test = $("<span>").html(text);
+        //adds a temporary element which contains the selected
+        //text to the body of the html document. It then measures
+        //the width of this new element and destroys it.
+        $temp_element.appendTo('body');
+          var width = $test.width();
+        $temp_element.remove();
 
-        // add to body, get width, and get out
-        $test.appendTo('body');
-        var width = $test.width();
-        $test.remove();
-
-        // set select width
-        $this.width(width + arrowWidth);
-
-        // run on start
+        $this.width(width);
       }).change();
-
     });
   };
-
-  // run by default
-  $("select.resizeselect").resizeselect();
-
 });
 
-//FINALLY FIGURED IT OUT HAHAHAHAHAHAHAHAHAH
-// 8-))
+//make sure this works with turbo links by calling the function
+//each time the page is "loaded" by turbolinks
 $(document).on("turbolinks:load", function(){
   return $("select.resizeselect").resizeselect();
 })
